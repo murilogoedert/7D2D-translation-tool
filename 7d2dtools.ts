8bd875d69966c8b965ba1d7571c4c5007f940a7b
murilo.goedert@omnismart.com.br <murilo.goedert@omnismart.com.br>
2024-11-05 19:52:13 -0300

    feat: initial commit

---
+++ 7d2dtools.ts
@@ -0,0 +1,224 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import * as progress from "cli-progress";
+import * as fs from "fs";
+import * as path from "path";
+import csv = require("csv-parser");
+
+const program = new Command();
+
+const languageMapping: { [lang: string]: string } = {
+  english: "en-US",
+  german: "de-DE",
+  spanish: "es-ES",
+  french: "fr-FR",
+  italian: "it-IT",
+  japanese: "ja-JP",
+  koreana: "ko-KR",
+  polish: "pl-PL",
+  brazilian: "pt-BR",
+  russian: "ru-RU",
+  turkish: "tr-TR",
+  schinese: "zh-CN",
+  tchinese: "zh-TW",
+};
+
+async function findLocalizationFiles(
+  dir: string,
+  fileList: string[] = []
+): Promise<string[]> {
+  const files = await fs.promises.readdir(dir);
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = await fs.promises.stat(filePath);
+    if (stat.isDirectory()) {
+      await findLocalizationFiles(filePath, fileList);
+    } else if (file === "Localization.txt") {
+      fileList.push(filePath);
+    }
+  }
+  return fileList;
+}
+
+async function dumpLanguage(args: any) {
+  const lang = (args.language || ("english" as string)).toLowerCase();
+  const targetLang = args.targetLanguage
+    ? args.targetLanguage.toLowerCase()
+    : false;
+  const googleTranslate = args.googleTranslate || false;
+
+  if (googleTranslate && !targetLang) {
+    console.error("Google Translate option requires a target language");
+    return;
+  }
+  if (!languageMapping[lang]) {
+    console.error(
+      "Invalid language selected. Available languages are: " +
+        Object.keys(languageMapping).join(", ")
+    );
+    return;
+  }
+
+  if (targetLang && !languageMapping[targetLang.toLowerCase()]) {
+    console.error(
+      "Invalid target language selected. Available languages are: " +
+        Object.keys(languageMapping).join(", ")
+    );
+    return;
+  }
+  const ignored = args.ignoreLanguage || false;
+  const ignoredFiles = [];
+  const filesWitError = [];
+  const localizationFiles = await findLocalizationFiles(
+    path.join(process.cwd(), "Mods")
+  );
+  if (localizationFiles.length === 0) {
+    console.error("No Localization.txt files found in the Mods directory.");
+    return;
+  }
+
+  const bar = new progress.SingleBar({}, progress.Presets.shades_classic);
+  bar.start(localizationFiles.length, 0);
+
+  let combinedData: { Key: string; Value: string }[] = [];
+  let count = 1;
+  for (const file of localizationFiles) {
+    let doIgnore = false;
+    const rows: { Key: string; Value: string }[] = [];
+    await new Promise((resolve, reject) => {
+      const parser = fs
+        .createReadStream(file)
+        .pipe(csv({ mapHeaders: ({ header }) => header.trim().toLowerCase() }));
+      parser.on("data", (data: any) => {
+        if (doIgnore) return;
+        if (ignored) {
+          const index = Object.keys(data).findIndex(
+            (key) => key.toLowerCase() == ignored.toLowerCase()
+          );
+          if (index !== -1 && data[Object.keys(data)[index]] != "") {
+            doIgnore = true;
+            return;
+          }
+        }
+        const index = Object.keys(data).findIndex(
+          (key) => key.toLowerCase() == lang.toLowerCase()
+        );
+
+        const indexUsedInMainMenu = Object.keys(data).findIndex(
+          (key) => key.toLowerCase() == "usedinmainmenu"
+        );
+
+        if (
+          (data.Key || data.key) &&
+          data[Object.keys(data)[index]] &&
+          !data[Object.keys(data)[indexUsedInMainMenu]]
+        ) {
+          let Value = "";
+          if (googleTranslate) {
+            Value = `=GOOGLETRANSLATE("${data[Object.keys(data)[index]].replace(
+              /"/g,
+              ""
+            )}"; "${languageMapping[lang]}"; "${
+              languageMapping[targetLang]
+            }" )`;
+          } else {
+            Value = `"${data[Object.keys(data)[index]].replace(/"/g, "")}"`;
+          }
+          rows.push({
+            Key: data.Key || data.key,
+            Value,
+          });
+        }
+      });
+      parser.on("end", () => resolve(rows));
+      parser.on("error", reject);
+    }).catch((err) => {
+      console.log(err);
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 10));
+
+    if (rows.length === 0) {
+      if (doIgnore) {
+        ignoredFiles.push(file);
+      } else {
+        filesWitError.push(file);
+      }
+      bar.update(count++);
+      continue;
+    }
+    combinedData = combinedData.concat(rows);
+    bar.update(count++);
+  }
+  combinedData.sort((a, b) => a.Key.localeCompare(b.Key));
+
+  combinedData.unshift({ Key: "Key", Value: targetLang || lang });
+  const outputContent = combinedData
+    .map((row) => `${row.Key}${googleTranslate ? "º" : ","}${row.Value}`)
+    .join("\n");
+  await fs.promises.writeFile(
+    path.join(process.cwd(), "Localization-DUMP.txt"),
+    outputContent,
+    "utf8"
+  );
+  bar.stop();
+  console.log("\nLocalization file created at ./Localization-DUMP.txt");
+  if (ignoredFiles.length > 0) {
+    console.log(
+      "\nFiles ignored because of the ignored language (" + ignored + ")"
+    );
+    console.table(
+      ignoredFiles.map((file) => ({ File: file })),
+      ["File"]
+    );
+  }
+  if (filesWitError.length > 0) {
+    console.log(
+      "\nFiles with errors, should be checked manually for non existing selected language or malformed csv content:"
+    );
+    console.table(
+      filesWitError.map((file) => ({ File: file })),
+      ["File"]
+    );
+  }
+}
+
+program.name("7 Days to Die Utilities").version("0.0.1")
+  .description(`  ████████████████  
+  ████████████████  
+           ██████   
+          ███████   
+        ████████    
+       ████████     
+       ███████      
+      ████████      
+     ███████        
+     ███████        
+    ██████          
+   ███████          
+    ████            
+    ███             
+   █                `);
+
+program
+  .command("locale-dump")
+  .alias("ld")
+  .description("Dump the game Localization data to ./dump/Localization.txt")
+  .option("-l, --language [language]", "The language to dump", "english")
+  .option(
+    "-tl, --target-language [targetLanguage]",
+    "The target Language of the translation dump"
+  )
+  .option(
+    "-i, --ignore-language [ignored]",
+    "The language to ignore (If already has a translation for that language, do not dump)",
+    ""
+  )
+  .option(
+    "-gt, --google-translate [translate]",
+    "If the output must have googletranslate formula to easy translate via Google Sheets"
+  )
+
+  .action(dumpLanguage);
+
+program.parse();
--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# 7D2D-translation-tool
-A simple tool for 7D2D Mod translation
+
+
+**Have you ever tried translating a 7 Days to Die mod? The process can be incredibly tedious and time-consuming, but this tool is designed to save you a lot of time.**
+
+**Its primary feature compiles all localization files within the game’s 'Mods' folder into a single, organized file with just two columns: Key and your chosen Language. Additionally, it offers the option to inject a Google Sheets formula for Google Translate, potentially saving you days of work and leaving only the task of reviewing the final result.**
+
+This also enhances maintainability. With the file sorted alphabetically, tracking changes in the original localization files from mod creators becomes much easier. After an update, simply generate a new version, compare it with your previous one using a tool like WinMerge, and voilà—you can see exactly what’s changed!
+
+**If you like the project, please consider buying me a coffee using the "Donate" button <3**
+
+### Requirements
+
+- [**Node.js 20+**](https://nodejs.org/) must be installed on your machine.
+- **Operating System**: Windows
+
+### Installation
+
+1. Download the file and extract its contents to a preferred directory, such as **Program Files** (*If the files are deleted, the tool will cease to function*).
+2. Open the Terminal or PowerShell in this directory. (To do this, hold Shift + right-click the folder, and select "Open PowerShell window here.")
+3. Once inside the folder in the terminal, enter the following commands:
+
+   ```bash
+   npm install
+   ```
+
+   Then:
+
+   ```bash
+   npm install -g
+   ```
+
+Now the tool is installed on your machine.
+
+### Usage:
+
+To create a dump of all localization files from all mods, navigate to the 7 Days to Die root folder (where the "Mods" folder is located), open a terminal in this location, and type the following command:
+
+```bash
+7d2d ld
+```
+
+There are four parameters that you can pass to the `ld` command:
+
+- **`-l | --language`** (Optional) The original language that will be searched inside localization files to dump (Default is "English").
+- **`-tl | --target-language`** (Optional) The target language, used as the second column on the output dump, `-gt` uses this too.
+- **`-gt | --google-translate`** (Optional) Add the GOOGLETRANSLATE formula to each line based on the `language` and `target-language`.
+- **`-i | --ignore-language`** (Optional) Language to ignore, you can pass the target language of your translation, this way only files that haven’t yet been translated will be dumped.
+
+This will create a language dump file in the game root directory called **Localization-DUMP.txt**. This file will contain all localization texts from all mods in the game's Mods folder, organized into two columns: **Key** and **English** (or whatever language you dumped), sorted alphabetically. Now, you can translate each value and rename the English column to the target language of your choice.
+
+You can also keep versions of the dump file for future comparison when the mod author updates the localization files.
+
+**Note**: Typing `7d2d` in the terminal will bring up a help screen with a list of available commands.
+
+### Upcoming Features:
+
+- A merging tool to help visualize changes between mod releases.
+- A detector for malformed localization files.
+- A Modinfo.xml / Mod.zip generator.
+
+### Using Google Sheets to easily translate the file:
+
+You can take advantage of a feature in Google Sheets to automatically translate text for you. To do this, you'll need to use the `-gt` and `-tl` parameters in the command to specify the target language, and then apply the Google Translate formula to the output. This method allows you to easily translate all the values by importing the file into Google Sheets—completely free of charge!
+
+**Keep in mind that machine translation usually doesn't yield very good results, requiring manual review, but it's a good starting point.**
+
+1. First, run the command:
+
+   ```bash
+   7d2d ld -l {Language to translate from} -tl {Target language} -gt
+   ```
+
+2. Now the generated Localization-DUMP.txt lines should look like this:
+
+   ```text
+   Key,Language
+   ...
+   AC_MiningMachine2º=GOOGLETRANSLATE("Excavator (Finding Veins)"; "en-US"; "pt-BR" )
+   AC_MiningMachine3º=GOOGLETRANSLATE("Excavator (Starting Drill)"; "en-US"; "pt-BR" )
+   AC_MiningMachine4º=GOOGLETRANSLATE("Excavator (25% Full)"; "en-US"; "pt-BR" )
+   AC_MiningMachine5º=GOOGLETRANSLATE("Excavator (50% Full)"; "en-US"; "pt-BR" )
+   ...
+   ```
+
+   Note that the separator in this case is **º** instead of **,**. This is necessary to correctly import data into Google Sheets, ensuring that commas within text fields are used as separators (Sheets is pretty dumb sometimes).
+
+With this file in hand, open Google Sheets and click on "Import":
+
+![Google Sheets Import](https://i.imgur.com/GPTAVWx.jpg)
+
+Then you need to upload the file, and configure the import like this:
+
+![Google Sheets Config](https://i.imgur.com/kPQcveV.jpg)
+
+When you click "Import Data," Google Sheets will automatically begin the translation process. Depending on the number of rows in your file, this may take several minutes.
+
+**Note**: Occasionally, localization files from mod creators may contain malformed content (see the screenshot below). In such cases, you'll need to manually fix each malformed line by correcting the content or formula.
+
+![Malformed content screenshot](https://i.imgur.com/mmdwHbZ.jpg)
